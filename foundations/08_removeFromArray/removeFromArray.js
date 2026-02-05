613acc711040439c742da98d16ae1768de9ab87d
Ashish Jha <akjha0002@gmail.com>
2026-02-05 11:11:27 +0530

    Update removeFromArray.js
    completing the code

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+  return [...arr].reverse();
 };
 
-// Do not edit below this line
+let nums = [1, 3, 4, 5];
+console.log(removeFromArray(nums));
+console.log(nums);
+
 module.exports = removeFromArray;
