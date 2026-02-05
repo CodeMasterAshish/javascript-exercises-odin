baebf8a7151893e3ad35e255f67d48de4eedf475
Ashish Jha <akjha0002@gmail.com>
2026-02-05 11:01:23 +0530

    Refactor reverseString function for clarity
    Completing the code.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
 
-};
+console.log(reverseString("hello"));
 
-// Do not edit below this line
 module.exports = reverseString;
